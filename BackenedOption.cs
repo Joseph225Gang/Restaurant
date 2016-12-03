ff9546d0123c62d5e257829858b00b4f3aa4f706
JosephGang <lai225joseph@gmail.com>
2016-12-04 06:52:35 +0800

    Refactor

---
+++ BackenedOption.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MealRestaurant
+{
+    public static class BackenedOption
+    {
+        public static string EditCategory = "Edit Category";
+        public static string EditMeal = "Edit Meal";
+        public static string AddNewMeal = "Add New Meal";
+        public static string AddCategory = "Add Category";
+    }
+}
